77fc454541f423257b3f1d8ba818996d3ca03c62
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-03-08 14:05:47 +0200

    Add names to the phonebook - part 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
 import { useState } from "react";
+import "./App.css";
 
-function App() {}
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.id}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
